b74272e4735af6d0c4c33c503be467d8a687e08c
Alex <as_alexsandrov@mail.ru>
2026-05-27 17:51:38 +0300

    Удаление модели ContactInfo и соответствующего админ-класса

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
-from .models import ContactInfo
 
 
-@admin.register(ContactInfo)
-class ContactInfoAdmin(admin.ModelAdmin):
-    list_display = ('phone', 'email', 'address')
-    search_fields = ('phone', 'email', 'address')
-
-    def has_add_permission(self, request):
-        # Разрешить создание только одной записи
-        if self.model.objects.exists():
-            return False
-        return True
-
-    def has_delete_permission(self, request, obj=None):
-        # Запретить удаление, чтобы всегда была хотя бы одна запись
-        if self.model.objects.count() > 1:
-            return True
-        return False
